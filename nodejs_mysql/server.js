d093f4c511dcb3885945b3779dc77573b4224217
Emir Faisal <12749535+emfaisal@users.noreply.github.com>
2023-08-20 00:56:37 +0700

    Add example nodejs app

---
+++ nodejs_mysql/server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const express = require('express');
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "mariadb.mariadb.svc.cluster.local",
+  user: "root",
+  password: "DCtGLNjYzQ"
+});
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    res.send('Hello World\n' + con.connect);
+  });
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
+
+/*
+docker build . -t node-web-app
+docker tag node-web-app localhost:5000/node-web-app
+docker push localhost:5000/node-web-app
+kubectl rollout restart deployment node-web-app
+kubectl get pods
+*/
